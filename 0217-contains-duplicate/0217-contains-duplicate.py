5714537fa6cfb0c90ef7b3361ce092a0e681d468
Rahul Varun <rahulvarunnnn@gmail.com>
2026-09-02 20:54:44 +0530

    Time: 104 ms (5.09%), Space: 20.1 MB (95.14%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,11 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        temp=set()
-        for i in nums:
-            if i in temp:
+        nums.sort()
+        for i in range(0,len(nums)-1):
+            if nums[i]==nums[i+1]:
                 return True
-            temp.add(i)
-        
         return False
+
+        #approch 2.: using set
+        # temp=set()
+        # for i in nums:
+        #     if i in temp:
+        #         return True
+        #     temp.add(i)
+        
+        # return False
         
